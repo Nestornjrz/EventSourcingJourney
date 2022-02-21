d44b71554b8ed9d2870822aac4043cbe6d49caa9
nestornjrz <nestornjrz@hotmail.com>
2022-02-21 09:46:46 -0300

    Clase 1, commad loop with args

---
+++ src/DemoHost/Domain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoHost
+{
+    public static class Domain
+    {
+        public class Commands
+        {
+            public const string Add = "add";
+            public const string Remove = "remove";
+        }
+
+        public class Queries
+        {
+            public const string GetQuantity = "getquantity";
+        }
+    }
+}
---
+++ src/DemoHost/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DemoHost
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cmd = Console.ReadLine()?.ToLower();
+
+
+            while (cmd != "exit")
+            {
+                var tokens = cmd?.Split(" ");
+                string? arg = null;
+
+                if(tokens is not null && tokens.Length == 2)
+                {
+                    cmd = tokens[0];
+                    arg = tokens[1];
+                }
+
+                switch (cmd)
+                {
+                    case Domain.Commands.Add:
+                        Console.WriteLine($"Product Added {arg}");
+                        break;
+                    case Domain.Commands.Remove:
+                        Console.WriteLine($"Product Removed {arg}");
+                        break;
+                    case Domain.Queries.GetQuantity:
+                        Console.WriteLine($"Quantity Displayed {arg}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command try again");
+                        break;
+                }
+                cmd = Console.ReadLine()?.ToLower();
+            }
+        }
+    }
+}
